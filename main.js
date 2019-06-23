3fed7f39076175d676dc9b4500f3ada07fb618bd
Marcin Sawicki <marcin.r.sawicki@gmail.com>
2019-06-23 12:06:27 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,10 @@
+console.log('siema');
+const name = 'Maciek Korsan';
+const age = 31;
+console.log(name);
+console.log(age);
+
+console.log(`Nazywam się ${name} 
+i mam ${age} lat`);
+
+console.log('Nazywam się '+name+' i mam '+age+' lat');
